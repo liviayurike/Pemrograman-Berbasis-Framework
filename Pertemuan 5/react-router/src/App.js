d0c599591c774f29d832d34339378aad6632f556
liviayurike <kliviayurike@gmail.com>
2021-03-10 12:53:22 +0700

    Source Code

--- Pertemuan 5/react-router/src/App.js
+++ Pertemuan 5/react-router/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import{
   BrowserRouter as Router,
   Switch,
@@ -134,4 +134,227 @@ function LoginPage() {
       <button onClick={login}>Log in</button>
     </div>
   );
+} 
+
+/*--------------------------------------------------------------------------------------------*/
+/*
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
+
+// Karena route adalah komponen regular react,
+// sehingga dapat ditampilkan di mana saja dalam penempatannya,
+// termasuk dalam child element/
+
+// Ini akan membantu anda untuk memecah menjadi beberapa bundel
+// karena pemisahan kode pada aplikasi react router sama dengan
+// pemecahan kode pada aplikasi react lainnya.
+
+export default function NestingExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Topics() {
+  // `path` untuk membuat jalur <Route> yang terhadap rute induk,
+  // sedangkan `url` untuk membuat link.
+
+  let { path, url } = useRouteMatch();
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/Sate, Nasi Goreng`}>Kuliner</Link>
+        </li>
+        <li>
+          <Link to={`${url}/Wisata alam, Museum`}>Travelling</Link>
+        </li>
+        <li>
+          <Link to={`${url}/Ibis, JW Matriot`}>Review Hotel</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+        <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+
+  return (
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}/*
+
+/*--------------------------------------------------------------------------------------------*/
+/*
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+
+// Params adalah placeholder di URL yang dimulai dengan titik dua,
+// seperti param `: id` yang didefinisikan dalam route dalam contoh ini.
+export default function ParamsExample() {
+  return (
+    <Router>
+      <div>
+        <h2>Accounts</h2>
+        <ul>
+          <li>
+            <Link to="/netflix">Netflix</Link>
+          </li>
+          <li>
+            <Link to="/gmail">Gmail</Link>
+          </li>
+          <li>
+            <Link to="/yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/amazon">Amazon</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/:id" children={<Child />} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Child() {
+  let { id } = useParams();
+
+  return (
+    <div>
+      <h3>ID: {id}</h3>
+    </div>
+  );
+} */
+
+/*--------------------------------------------------------------------------------------------*/
+/*
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// Situs ini memiliki 3 halaman, yang semuanya dirender secara dinamis di browser
+// Meskipun halam tidak pernah di-refresh, perhatikan bagaimana React router
+// membuat URL selalu terbarui saat anda menavigasi situs,
+// Ini menjaga riwayat browser, memastikan hal-hal seperti tombol kembali
+// dan bookmark berfungsi dengan baik
+
+export default function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <hr />
+
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// anda dapat menganggap komponen ini sebagai "halaman(konten)" di situs anda.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}*/
